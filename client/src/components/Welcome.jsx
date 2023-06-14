867828b9dffaaa39339e172387158bd401fdf344
Licol Zeinfeld <licol@berkeley.edu>
2023-06-13 14:07:33 -1000

    md

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -22,9 +22,10 @@ const Welcome = () => {
 
     }
 
+
     return (
         <div className="flex w-full justify-center items-center"> 
-            <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+            <div className="flex mf:flex-row flex-col items-start justify-between mf:p-20 py-12 px-4">
                 <div className="flex flex-1 justify-start flex-col md:mr-10">
                     <h1 className="text-3xl sm:text-4xl text-white text-gradient">
                         Send Crypto <br /> across the globe
@@ -60,7 +61,7 @@ const Welcome = () => {
                     </div>
                 </div>
 
-                <div className= "flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10">
+                <div className= "flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
                     <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism border-0">
                         <div className="flex justify-between flex-col w-full height-full">
                             <div className="flex justify-between items-start">
@@ -85,6 +86,20 @@ const Welcome = () => {
                         <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={() => {}} />
                         <Input placeholder="Key Word (Gif)" name="keyWord" type="text" handleChange={() => {}} />
                         <Input placeholder="Enter Message" name="message" type="text" handleChange={() => {}} />
+                    
+                        <div className="h-[1px] w-full bg-gray-400 my-2" />
+
+                        {true ? (
+                            <Loader />
+                        ) : (
+                            <button
+                            type="button"
+                            onClick={handleSubmit}
+                            className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer"
+                            >
+                            Send Now
+                            </button>
+                        )}
                     </div>
 
                 </div>
